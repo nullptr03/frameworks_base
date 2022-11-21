acf70c9c0288acc7342cb9e42be162ffc8818c77
Ido Ben-Hur <idoybh2@gmail.com>
2022-11-21 16:48:45 +0200

    base: Allow customizing default notification vibration pattern [1/2]
    Re-use the patterns we've got for ringtone vibration as options
    In addition allow crafting a custom pattern same as we do for ringtone

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -102,5 +102,7 @@ public class SystemSettings {
         Settings.System.SCREEN_FLASH_NOTIFICATION_COLOR,
         Settings.System.RINGTONE_VIBRATION_PATTERN,
         Settings.System.CUSTOM_RINGTONE_VIBRATION_PATTERN,
+        Settings.System.NOTIFICATION_VIBRATION_PATTERN,
+        Settings.System.CUSTOM_NOTIFICATION_VIBRATION_PATTERN,
     };
 }
